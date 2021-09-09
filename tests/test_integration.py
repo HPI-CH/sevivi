2df1ed31c57c560524e84878ae75830fa4f30671
enra64 <enra64@users.noreply.github.com>
2021-09-09 18:04:52 +0200

    add (failing) new test

---
+++ tests/test_integration.py
@@ -0,0 +1,7 @@
+from sevivi import video_renderer_from_csv_files, read_configs
+
+
+def test_plain_video_render(run_in_repo_root):
+    config = read_configs(("test_files/test-data-configs/manual_sync.toml",))
+    video_renderer = video_renderer_from_csv_files(config)
+    video_renderer.render_video()
