cc196fa61d72bee986a2d814ba4059f8ad038444
enra64 <enra64@users.noreply.github.com>
2021-09-09 09:35:32 +0200

    remove ImageProvider - we're not subclassing that anymore

--- sevivi/image_provider/image_provider.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-
-class ImageProvider:
-    def get_image_for_time_stamp(self, ts: pd.Timestamp):
-        raise NotImplementedError(
-            "ImageProvider subclasses need to provide images for TS!"
-        )
